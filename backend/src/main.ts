d0be32e4c3c3f8354de1dfa643d21f5d08ce0a61
Gustavo Perez <gperez@tradestation.com>
2025-07-21 21:18:07 -0600

    Add railway config file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim())
+  : 'http://localhost:4000';
+
   app.enableCors({
-    origin: [process.env.CORS_ORIGIN ?? 'http://localhost:4000'],
+    origin: allowedOrigins,
     credentials: true,
   });
-
   const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`✅ App is running on port ${port}`);
